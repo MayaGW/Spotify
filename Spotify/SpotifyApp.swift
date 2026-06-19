c113aed9e55c56defcb5c4f71c5116e068f3b8be
Maya Ghamloush <maya.ghamloushe@gmail.com>
2026-06-19 11:06:10 +0300

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Maya Ghamloush on 19/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
