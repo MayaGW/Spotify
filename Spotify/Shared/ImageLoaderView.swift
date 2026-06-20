76f0a88e895f7e38bda886cc3e18302819880cd9
Maya Ghamloush <maya.ghamloushe@gmail.com>
2026-06-20 11:34:50 +0300

    Create a Constant File and WebImage Testing

--- Spotify/Shared/ImageLoaderView.swift
+++ Spotify/Shared/ImageLoaderView.swift
@@ -6,10 +6,14 @@
 //
 
 import SwiftUI
-
+import SDWebImageSwiftUI
 struct ImageLoaderView: View {
+    
+    var urlString: String = Constants.randomImage
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebImage(url: URL(string: urlString))
+            
     }
 }
 
