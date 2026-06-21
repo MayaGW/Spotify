16817d5d38907eb1ee50e66adb1e7519b3c23a00
Maya Ghamloush <maya.ghamloushe@gmail.com>
2026-06-21 15:31:26 +0300

    Update Product Model And make it conform Identifiable

--- Spotify/Shared/Product.swift
+++ Spotify/Shared/Product.swift
@@ -9,17 +9,16 @@ import Foundation
 
 struct ProductArray: Codable {
     let products: [Product]
-    let total, skip, limit: Int?
+    let total, skip, limit: Int
 }
 
 // MARK: - Product
-struct Product: Codable {
-    let id: Int?
-    let title, description: String?
-    //let category: Category?
-    let price, discountPercentage, rating: Double?
-    let stock: Int?
-    let brand, sku: String?
-    let images: [String]?
-    let thumbnail: String?
+struct Product: Codable, Identifiable {
+    let id: Int
+    let title, description: String
+    let price, discountPercentage, rating: Double
+    let stock: Int
+    let brand, sku: String
+    let images: [String]
+    let thumbnail: String
 }
