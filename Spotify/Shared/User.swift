41ca405b4c9beba80162775adebab510a220ee92
Maya Ghamloush <maya.ghamloushe@gmail.com>
2026-06-21 14:49:56 +0300

    Create User Model in separate file

---
+++ Spotify/Shared/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  Spotify
+//
+//  Created by Maya Ghamloush on 21/06/2026.
+//
+
+import Foundation
+
+struct UserArray: Codable {
+    let users: [User]
+    let total, skip, limit: Int?
+}
+
+// MARK: - User
+struct User: Codable {
+    let id: Int
+    let firstName, lastName: String
+    let age: Int
+    let email, phone, username, password: String
+    let image: String
+    let height, weight: Double
+  
+}
