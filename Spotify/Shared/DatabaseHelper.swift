bb8852d1b0721ee936d3ed98043bda19fd488e4c
Maya Ghamloush <maya.ghamloushe@gmail.com>
2026-06-21 14:50:30 +0300

    Remove the User and Product Model into separate files

--- Spotify/Shared/DatabaseHelper.swift
+++ Spotify/Shared/DatabaseHelper.swift
@@ -18,23 +18,18 @@ struct DatabaseHelper{
         let products = try JSONDecoder().decode(ProductArray.self, from: data)
         return products.products
     }
+    func getUsers() async throws -> [User]{
+        guard let url = URL(string: "https://dummyjson.com/users") else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let users = try JSONDecoder().decode(UserArray.self, from: data)
+        return users.users
+    }
     
 }
 
-struct ProductArray: Codable {
-    let products: [Product]
-    let total, skip, limit: Int?
-}
 
-// MARK: - Product
-struct Product: Codable {
-    let id: Int?
-    let title, description: String?
-    //let category: Category?
-    let price, discountPercentage, rating: Double?
-    let stock: Int?
-    let brand, sku: String?
-    let images: [String]?
-    let thumbnail: String?
-}
  
+
